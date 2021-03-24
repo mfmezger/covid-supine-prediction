543518bcbc17eba5dc6eb2479f6e243701fcb326
marc.mezger <marc.mezger@uni-ulm.de>
2021-03-24 18:39:50 +0100

    Adding first network and scripting.

---
+++ training/network.py
@@ -0,0 +1,76 @@
+import pytorch_lightning as pl
+import torch
+import torch.nn.functional as F
+from ranger_adabelief import RangerAdaBelief
+from torchvision import transforms
+from torch.utils.data import DataLoader
+from torchvision.datasets import  ImageFolder
+
+
+class LitClassifier(pl.LightningModule):
+
+    def __init__(self, hparams, training=False,  batch_size=32,
+                         train_path=None, val_path=None,
+                         test_path=None,  num_classes=2, learning_rate=1e-3):
+
+        super().__init__()
+        self.save_hyperparameters()
+        self.hparams.learning_rate = learning_rate
+        self.batch_size = batch_size
+        self.train_path = train_path
+        self.val_path = val_path
+        self.test_path = test_path
+
+    def forward(self, x):
+        # use forward for inference/predictions
+        embedding = self.backbone(x)
+        return embedding
+
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self.backbone(x)
+        loss = F.cross_entropy(y_hat, y)
+        self.log('train_loss', loss, on_epoch=True)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self.backbone(x)
+        loss = F.cross_entropy(y_hat, y)
+        self.log('valid_loss', loss, on_step=True)
+
+    def test_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self.backbone(x)
+        loss = F.cross_entropy(y_hat, y)
+        self.log('test_loss', loss)
+
+    def train_dataloader(self) -> DataLoader:
+        transform = transforms.Compose([
+            transforms.Resize((256, 256)),
+            transforms.RandomApply([
+                transforms.RandomRotation(15)
+            ]),
+            transforms.ToTensor(),
+            transforms.Normalize((0.48232,), (0.23051,))
+        ])
+        train = ImageFolder(self.train_path, transform=transform)
+        return DataLoader(train, batch_size=self.batch_size, shuffle=True)
+
+    def val_dataloader(self) -> DataLoader:
+        val = ImageFolder(self.val_path)
+        return DataLoader(val, batch_size=self.batch_size)
+
+    def test_dataloader(self) -> DataLoader:
+        test = ImageFolder(self.val_path)
+        return DataLoader(test, batch_size=self.batch_size)
+
+    def configure_optimizers(self):
+        # self.hparams available because we called self.save_hyperparameters()
+        return RangerAdaBelief(self.parameters(), lr=self.hparams.learning_rate, eps=1e-12, betas=(0.9, 0.999))
+
+    @staticmethod
+    def add_model_specific_args(parent_parser):
+        parser = parent_parser.add_argument_group("LitClassifier")
+        parser.add_argument('--learning_rate', type=float, default=0.0001)
+        return parent_parser
---
+++ training/training.py
@@ -0,0 +1,59 @@
+from argparse import ArgumentParser
+
+from pytorch_lightning import Trainer, seed_everything
+from pytorch_lightning.callbacks import ModelCheckpoint
+
+from torchvision.datasets import  ImageFolder
+from network import LitClassifier
+
+
+def main(hparams):
+    seed_everything(11)
+
+
+    # todo add WANDB.
+
+    train_dataset = ImageFolder(args.train_path)
+    val_dataset = ImageFolder(args.val_path)
+    test_dataset = ImageFolder(args.test_path)
+
+    # initialize the model
+    model = LitClassifier(training=True, hparams=hparams, batch_size=hparams.batch_size,
+                         train_dataset=train_dataset, val_dataset=val_dataset,
+                         test_dataset=test_dataset,  num_classes=2,learning_rate=hparams.learning_rate)
+
+    # initialize loggers
+    checkpoint_callback = ModelCheckpoint(filepath='pl_model/')
+
+    # Intialize the Trainer.
+    trainer = Trainer(gpus=1, num_nodes=1,
+                       profiler=True, min_epochs=1, max_epochs=hparams.max_epochs,
+                      checkpoint_callback=checkpoint_callback, benchmark=True, progress_bar_refresh_rate=20)
+
+    # start  the Training.
+    trainer.fit(model)
+
+    # activate testing
+    trainer.test(model)
+
+    trainer.save_checkpoint("model" + str(args.max_epochs) + "_" + str(args.learning_rate) + ".ckpt")
+
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument('--batch_size', default=2, type=int)
+    parser.add_argument('--learning_rate', default=0.01, type=float)
+    parser.add_argument('--num_workers', default=8, type=int)
+    parser.add_argument('--opt', default="ranger", type=str)
+    parser.add_argument('--loss', default="ce")
+    parser.add_argument('--augmentations', default=False, type=bool)
+    parser.add_argument('--max_epochs', default=1, type=int)
+    parser.add_argument('--train_path',
+                        default="/home/mfmezger/data/COVID/")
+    parser.add_argument('--val_path',
+                        default="/home/mfmezger/data/COVID/")
+    parser.add_argument('--test_path',
+                        default="/home/mfmezger/data/COVID/")
+    args = parser.parse_args()
+
+    main(args)
